d2d4decb525c763ce56afc961f994673700ed26d
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-05-10 22:48:40 +0200

    Ajout de la creation de loperation dans leventlistener

--- src/PiggyBox/TicketBundle/Listener/AccountListener.php
+++ src/PiggyBox/TicketBundle/Listener/AccountListener.php
@@ -3,17 +3,21 @@ namespace PiggyBox\TicketBundle\Listener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use PiggyBox\TicketBundle\Entity\Account;
+use PiggyBox\TicketBundle\Entity\Operation;
 
 class AccountListener
 {
     public function preUpdate(LifecycleEventArgs $args)
     {
-        $entity = $args->getEntity();
+        $account = $args->getEntity();
         $entityManager = $args->getEntityManager();
 
-        // perhaps you only want to act on some "Product" entity
-        if ($entity instanceof Account && $args->hasChangedField('balance')) {
-
+        if ($account instanceof Account && $args->hasChangedField('balance')) {
+        	$operation = new Operation();
+        	$operation->setAccount($operation);
+        	$operation->setNewBalance($args->getNewValue('balance'));
+        	$operation->setPreviousBalance($args->getNewValue('balance'));
+        	/*addOperation*/
         }
     }
 }
