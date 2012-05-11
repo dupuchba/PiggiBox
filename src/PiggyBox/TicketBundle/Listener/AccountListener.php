41708097eb15c8d657d9f0efd06e00090b553b5b
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-05-11 13:11:02 +0200

    Commit du listener

--- src/PiggyBox/TicketBundle/Listener/AccountListener.php
+++ src/PiggyBox/TicketBundle/Listener/AccountListener.php
@@ -9,40 +9,12 @@ use PiggyBox\TicketBundle\Entity\Account;
 use PiggyBox\TicketBundle\Entity\Operation;
 
 class AccountListener
-/*{
-    public function postUpdate(LifecycleEventArgs $args)
-    {
-        $account = $args->getEntity();
-        $entityManager = $args->getEntityManager();
-
-        if ($account instanceof Account ) {
-
-        	$account->setBalance($args->getNewValue('balance')-2);
-        	$operation = new Operation();
-        	$operation->setAccount($account);
-        	$operation->setNewBalance($args->getNewValue('balance'));
-        	$operation->setPreviousBalance($args->getOldValue('balance'));
-
-                $entityManager->persist($operation);
-                $entityManager->flush();
-        	//var_dump($operation);die();
-        	var_dump($operation);die();
-
-
-                $historicalStatus = new HistoricalStatus();
-                $historicalStatus->setOrder($entity);
-                $historicalStatus->setComment($entity-&gt;getComment());
-                $historicalStatus->setStatus($entity-&gt;getStatus());
-                $entityManager->persist($historicalStatus);
-                $entityManager->flush();
-        }
-    }
-}*/
-
 {
+
         private $em = null;
         private $uow = null;
         private $attachedEvents;
+
 
         public function onFlush(OnFlushEventArgs $args) {
 
@@ -68,9 +40,7 @@ class AccountListener
 
                                 $this->em->persist($operation);
                                 $this->em->flush();
-
-                        
-
+                                
                         // recalculate changeset, since we might messed up the relations
                         $this -> em -> getUnitOfWork() -> recomputeSingleEntityChangeSet($meta, $entity);
                         }
